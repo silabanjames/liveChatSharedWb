8660a6f17de36cd336b3718d89071194558c950c
Muhammad Fath Mushaffa Azhar <87472508+fshaaa@users.noreply.github.com>
2022-03-12 19:05:21 +0700

    update whiteboard

--- WBoard/public/script.js
+++ WBoard/public/script.js
@@ -1,7 +1,21 @@
 let canvas = document.getElementById("canvas")
+let colors = document.getElementsByClassName("color")
 
-canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+canvas.width = window.innerWidth 
+
+var current = {
+    color: 'black'
+}
+
+for (var i = 0; i < colors.length; i++){
+    colors[i].addEventListener('click', onColorUpdate, false);
+}
+
+function onColorUpdate(e){
+    current.color = e.target.className.split(' ')[1];
+}
+
 
 let ctx = canvas.getContext("2d")
 
@@ -9,6 +23,7 @@ var io = io.connect('http://localhost:9000/')
 
 let x
 let y
+let warna = current.color
 let mouseDown = false
 
 window.onmousedown = (e) =>{
@@ -25,18 +40,20 @@ io.on("ondown", ({x, y}) => {
     ctx.moveTo(x, y)
 })
 
-io.on("ondraw", ({x, y}) => {
+io.on("ondraw", ({x, y, warna}) => {
     ctx.lineTo(x, y)
-    ctx.stroke()
+    ctx.stroke().strokeStyle = warna
 })
 
 window.onmousemove = (e) => {
     x = e.clientX
     y = e.clientY
+    warna = current.color
 
     if (mouseDown) {
         io.emit("draw", {x, y})
         ctx.lineTo(x, y)
         ctx.stroke()
+        ctx.strokeStyle = warna
     }
 }
